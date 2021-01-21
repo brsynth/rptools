0366f4de455cf460832981fedd97101f4fd06ea6
breakthewall <joan.herisson@univ-evry.fr>
2021-01-21 09:54:46 +0100

    build(setup.py): cleanup

--- setup.py
+++ setup.py
@@ -2,55 +2,76 @@ from setuptools import setup
 from re import search as re_search
 from os import path as os_path
 
-_readme = 'README.md'
 
-with open(_readme, 'r', encoding='utf-8') as f:
-    long_description = f.read()
+## README
+def read_readme(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        return f.read()
 
-_release = 'RELEASE'
 
-# with open(_release, 'r') as f:
-#     _version = f.readline().split()[0]
-with open(_release, 'r') as f:
-    line = f.readline()
-    while line:
-        match = re_search("^## (\d\.\d\.\d)$", line)
-        print(match)
-        if match:
-            _version = match.group(1)
-            break
+## RELEASE
+def get_version(filename):
+    with open(filename, 'r') as f:
         line = f.readline()
+        while line:
+            match = re_search("^## (\d\.\d\.\d)$", line)
+            if match:
+                return match.group(1)
+            line = f.readline()
 
-_extras_path = 'extras'
-with open(os_path.join(_extras_path, '.env'), 'r', encoding='utf-8') as f:
-    for line in f:
-        if line.startswith('PACKAGE='):
-            _package = line.splitlines()[0].split('=')[1].lower()
-        if line.startswith('URL='):
-            _url = line.splitlines()[0].split('=')[1].lower()
-        if line.startswith('AUTHORS='):
-            _authors = line.splitlines()[0].split('=')[1].lower()
-        if line.startswith('DESCR='):
-            _descr = line.splitlines()[0].split('=')[1].lower()
-        if line.startswith('CORR_AUTHOR='):
-            _corr_author = line.splitlines()[0].split('=')[1].lower()
 
-with open(os_path.join(_package, '.env'), 'r', encoding='utf-8') as f:
-    for line in f:
-        if line.startswith('MODULES='):
-            _modules = line.splitlines()[0].split('=')[1].lower().split(',')
-            _packages = [_package] + [_package+'.'+m for m in _modules]
+## EXTRAS INFOS
+def get_extras(filename):
 
+    extras_infos = {
+        'package'      : '',
+        'url'          : '',
+        'autors'       : '',
+        'corr_authors' : '',
+        'descr'  : ''
+    }
+
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            if line.startswith('PACKAGE='):
+                extras_infos['package'] = line.splitlines()[0].split('=')[1].lower()
+            if line.startswith('URL='):
+                extras_infos['url'] = line.splitlines()[0].split('=')[1].lower()
+            if line.startswith('AUTHORS='):
+                extras_infos['authors'] = line.splitlines()[0].split('=')[1].lower()
+            if line.startswith('DESCR='):
+                extras_infos['descr'] = line.splitlines()[0].split('=')[1].lower()
+            if line.startswith('CORR_AUTHOR='):
+                extras_infos['corr_authors'] = line.splitlines()[0].split('=')[1].lower()
+
+    return extras_infos
+
+
+## PACKAGES NAMES
+def get_packages(package):
+    with open(os_path.join(package, '.env'), 'r', encoding='utf-8') as f:
+        for line in f:
+            if line.startswith('MODULES='):
+                modules  = line.splitlines()[0].split('=')[1].lower().split(',')
+                packages = [package] + [package+'.'+m for m in modules]
+    return packages
+
+
+# get extras infos
+extras_infos = get_extras(os_path.join('extras', '.env'))
+
+
+## SETUP
 setup(
-    name                          = _package,
-    version                       = _version,
-    author                        = _authors,
-    author_email                  = _corr_author,
-    description                   = _descr,
-    long_description              = long_description,
+    name                          = extras_infos['package'],
+    version                       = get_version('RELEASE'),
+    author                        = extras_infos['authors'],
+    author_email                  = extras_infos['corr_authors'],
+    description                   = extras_infos['descr'],
+    long_description              = read_readme('README.md'),
     long_description_content_type = 'text/markdown',
-    url                           = _url,
-    packages                      = _packages,
+    url                           = extras_infos['url'],
+    packages                      = get_packages(extras_infos['package']),
     include_package_data          = True,
     test_suite                    = 'pytest',
     license                       = 'MIT',
