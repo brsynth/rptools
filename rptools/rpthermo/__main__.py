7bd92dd83e565daa4bc96dba893f3adebbee95ee
breakthewall <joan.herisson@univ-evry.fr>
2021-06-24 18:23:27 +0200

    chore(rpthermo): write results to pathway in runThermo()

--- rptools/rpthermo/__main__.py
+++ rptools/rpthermo/__main__.py
@@ -54,14 +54,8 @@ def _cli():
         logger=logger
     )
 
-    # results = {'net_reaction': {'dG0_prime': {'value': -789.8933819367826, 'error': 7.7248778091405255, 'units': 'kilojoule / mole'}, 'dGm_prime': {'value': -789.8933819367826, 'error': 7.7248778091405255, 'units': 'kilojoule / mole'}, 'dG_prime': {'value': -789.8933819367826, 'error': 7.7248778091405255, 'units': 'kilojoule / mole'}}, 'reactions': {'rxn_2': {'dG0_prime': {'value': -324.1942486194258, 'error': 5.946918851751192, 'units': 'kilojoule / mole'}, 'dGm_prime': {'value': -307.0794110847048, 'error': 5.946918851751192, 'units': 'kilojoule / mole'}, 'dG_prime': {'value': -324.1942486194258, 'error': 5.946918851751192, 'units': 'kilojoule / mole'}}, 'rxn_1': {'dG0_prime': {'value': -465.6991333173571, 'error': 5.39929292564583, 'units': 'kilojoule / mole'}, 'dGm_prime': {'value': -482.8139708520781, 'error': 5.39929292564583, 'units': 'kilojoule / mole'}, 'dG_prime': {'value': -465.6991333173571, 'error': 5.39929292564583, 'units': 'kilojoule / mole'}}}, 'species': {'MNXM188': {'standard_dg_formation': {'value': -201.33117647305943, 'units': 'kilojoule / mole'}}, 'MNXM15': {'standard_dg_formation': {'value': -81.91776181003092, 'units': 'kilojoule / mole'}}, 'MNXM4': {'standard_dg_formation': {'value': 16.399999999865035, 'units': 'kilojoule / mole'}}, 'MNXM6': {'standard_dg_formation': {'value': -3070.5260288612576, 'units': 'kilojoule / mole'}}, 'CMPD_0000000003': {'standard_dg_formation': {'value': -290.7486822708706, 'units': 'kilojoule / mole'}}, 'TARGET_0000000001': {'standard_dg_formation': {'value': -508.17794736445586, 'units': 'kilojoule / mole'}}, 'MNXM13': {'standard_dg_formation': {'value': -386.0000000000019, 'units': 'kilojoule / mole'}}, 'MNXM5': {'standard_dg_formation': {'value': -3098.931269475794, 'units': 'kilojoule / mole'}}, 'MNXM1': {'standard_dg_formation': {'value': None, 'units': 'kilojoule / mole'}}}}
-
     # Print results
     print_results(pathway, results, logger)
-
-    # Write results into pathway
-    write_results(pathway, results, logger)
-
     # Write pathway into file
     rpSBML.from_Pathway(pathway).write_to_file(args.outfile)
     logger.info(
@@ -73,32 +67,6 @@ def _cli():
         )
     )
 
-
-def write_results(
-  pathway: rpPathway,
-  results: Dict,
-  logger: Logger = getLogger(__name__)
-) -> None:
-    # Write species results
-    for spe_id, score in results['species'].items():
-        for k, v in score.items():
-            pathway.get_specie(spe_id).set_thermo_info(
-                key=k,
-                value=v
-            )
-    # Write reactions results
-    for rxn_id, score in results['reactions'].items():
-        for k, v in score.items():
-            pathway.get_reaction(rxn_id).set_thermo_info(
-                key=k,
-                value=v
-            )
-    # Write pathway result
-    for k, v in results['net_reaction'].items():
-        pathway.set_thermo_info(
-        key=k,
-        value=v
-        )
 
 def print_results(
     pathway: rpPathway,
