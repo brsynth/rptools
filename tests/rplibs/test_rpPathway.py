f91d1dacc6f27d5d82c44c86bd9cc39bd698c198
breakthewall <joan.herisson@univ-evry.fr>
2021-08-27 10:02:10 +0200

    test(rplibs): define species within setUp()

--- tests/rplibs/test_rpPathway.py
+++ tests/rplibs/test_rpPathway.py
@@ -4,7 +4,6 @@ Created on May 28 2021
 @author: Joan Hérisson
 """
 
-from os import path as os_path
 from tempfile import NamedTemporaryFile
 from unittest import TestCase
 from copy import deepcopy
@@ -18,92 +17,93 @@ from rptools.rplibs import (
 
 class Test_rpPathway(TestCase):
 
-    species = {
-        "TARGET_0000000001": rpCompound(
-            id="TARGET_0000000001",
-            smiles="[H]OC(=O)C([H])=C([H])C([H])=C([H])C(=O)O[H]",
-            inchi="InChI=1S/C6H6O4/c7-5(8)3-1-2-4-6(9)10/h1-4H,(H,7,8)(H,9,10)",
-            inchikey="TXXHDPDFNKHHGW-UHFFFAOYSA-N"
-        ),
-        "CMPD_0000000010": rpCompound(
-            id="CMPD_0000000010",
-            smiles="[H]OC(=O)c1c([H])c([H])c(O[H])c(O[H])c1[H]",
-            inchi="InChI=1S/C7H6O4/c8-5-2-1-4(7(10)11)3-6(5)9/h1-3,8-9H,(H,10,11)",
-            inchikey="YQUVCSBJEUQKSH-UHFFFAOYSA-N"
-        ),
-        "MNXM23": rpCompound(
-            id="MNXM23",
-            formula="C3H3O3",
-            smiles="CC(=O)C(=O)O]",
-            inchi="InChI=1S/C3H4O3/c1-2(4)3(5)6/h1H3,(H,5,6)",
-            inchikey="LCTONWCANYUPML-UHFFFAOYSA-N",
-            name="pyruvate"
-        ),
-        "CMPD_0000000025": rpCompound(
-            id="CMPD_0000000025",
-            smiles="[H]OC(=O)c1c([H])c([H])c([H])c(O[H])c1[H]",
-            inchi="InChI=1S/C7H6O3/c8-6-3-1-2-5(4-6)7(9)10/h1-4,8H,(H,9,10)",
-            inchikey="IJFXRHURBJZNAO-UHFFFAOYSA-N"
-        ),
-        "CMPD_0000000003": rpCompound(
-            id="CMPD_0000000003",
-            smiles="[H]Oc1c([H])c([H])c([H])c([H])c1O[H]",
-            inchi="InChI=1S/C6H6O2/c7-5-3-1-2-4-6(5)8/h1-4,7-8H",
-            inchikey="YCIMNLLNPGFGHC-UHFFFAOYSA-N"
-        ),
-        "MNXM337": rpCompound(
-            id="MNXM337",
-            smiles="[H]OC(=O)C(OC1([H])C([H])=C(C(=O)O[H])C([H])=C([H])C1([H])O[H])=C([H])[H]",
-            inchi="InChI=1S/C10H10O6/c1-5(9(12)13)16-8-4-6(10(14)15)2-3-7(8)11/h2-4,7-8,11H,1H2,(H,12,13)(H,14,15)",
-            inchikey="WTFXTQVDAKGDEY-UHFFFAOYSA-N"
-        ),
-        "MNXM2": rpCompound(
-            id="MNXM2",
-            smiles="[H]O[H]",
-            inchi="InChI=1S/H2O/h1H2",
-            inchikey="XLYOFNOQVPJJNP-UHFFFAOYSA-N"
-        ),
-        "MNXM13": rpCompound(
-            id="MNXM13",
-            smiles="O=C=O",
-            inchi="InChI=1S/CO2/c2-1-3",
-            inchikey="CURLTUGMZLYLDI-UHFFFAOYSA-N",
-            formula="CO2",
-            name="CO2"
-        ),
-        "MNXM5": rpCompound(
-            id="MNXM5",
-            smiles="N=C(O)c1ccc[n+](C2OC(COP(=O)(O)OP(=O)(O)OCC3OC(n4cnc5c(N)ncnc54)C(OP(=O)(O)O)C3O)C(O)C2O)c1",
-            inchi="InChI=1S/C21H28N7O17P3/c22-17-12-19(25-7-24-17)28(8-26-12)21-16(44-46(33,34)35)14(30)11(43-21)6-41-48(38,39)45-47(36,37)40-5-10-13(29)15(31)20(42-10)27-3-1-2-9(4-27)18(23)32/h1-4,7-8,10-11,13-16,20-21,29-31H,5-6H2,(H7-,22,23,24,25,32,33,34,35,36,37,38,39)/p+1",
-            inchikey="XJLXINKUBYWONI-UHFFFAOYSA-O",
-            formula="C21H25N7O17P3",
-            name="NADP(+)"
-        ),
-        "MNXM4": rpCompound(
-            id="MNXM4",
-            smiles="O=O",
-            inchi="InChI=1S/O2/c1-2",
-            inchikey="MYMOFIZGZYHOMD-UHFFFAOYSA-N"
-        ),
-        "MNXM1": rpCompound(
-            id="MNXM1",
-            smiles="[H+]",
-            inchi="InChI=1S/p+1",
-            inchikey="GPRLSGONYQIRFK-UHFFFAOYSA-N"
-        ),
-        "MNXM6": rpCompound(
-            id="MNXM6",
-            smiles="[H]N=C(O[H])C1=C([H])N(C2([H])OC([H])(C([H])([H])OP(=O)(O[H])OP(=O)(O[H])OC([H])([H])C3([H])OC([H])(n4c([H])nc5c(N([H])[H])nc([H])nc54)C([H])(OP(=O)(O[H])O[H])C3([H])O[H])C([H])(O[H])C2([H])O[H])C([H])=C([H])C1([H])[H]",
-            inchi="InChI=1S/C21H30N7O17P3/c22-17-12-19(25-7-24-17)28(8-26-12)21-16(44-46(33,34)35)14(30)11(43-21)6-41-48(38,39)45-47(36,37)40-5-10-13(29)15(31)20(42-10)27-3-1-2-9(4-27)18(23)32/h1,3-4,7-8,10-11,13-16,20-21,29-31H,2,5-6H2,(H2,23,32)(H,36,37)(H,38,39)(H2,22,24,25)(H2,33,34,35)",
-            inchikey="ACFIXJIJDZMPPO-UHFFFAOYSA-N"
-        )
-    }
-
     def setUp(self):
         self.target = rpCompound(
             id='TARGET_0000000001',
             smiles='[H]OC(=O)C([H])=C([H])C([H])=C([H])C(=O)O[H]'
         )
+        species = {
+            # "TARGET_0000000001": rpCompound(
+            #     id="TARGET_0000000001",
+            #     smiles="[H]OC(=O)C([H])=C([H])C([H])=C([H])C(=O)O[H]",
+            #     inchi="InChI=1S/C6H6O4/c7-5(8)3-1-2-4-6(9)10/h1-4H,(H,7,8)(H,9,10)",
+            #     inchikey="TXXHDPDFNKHHGW-UHFFFAOYSA-N"
+            # ),
+            "TARGET_0000000001": self.target,
+            "CMPD_0000000010": rpCompound(
+                id="CMPD_0000000010",
+                smiles="[H]OC(=O)c1c([H])c([H])c(O[H])c(O[H])c1[H]",
+                inchi="InChI=1S/C7H6O4/c8-5-2-1-4(7(10)11)3-6(5)9/h1-3,8-9H,(H,10,11)",
+                inchikey="YQUVCSBJEUQKSH-UHFFFAOYSA-N"
+            ),
+            "MNXM23": rpCompound(
+                id="MNXM23",
+                formula="C3H3O3",
+                smiles="CC(=O)C(=O)O]",
+                inchi="InChI=1S/C3H4O3/c1-2(4)3(5)6/h1H3,(H,5,6)",
+                inchikey="LCTONWCANYUPML-UHFFFAOYSA-N",
+                name="pyruvate"
+            ),
+            "CMPD_0000000025": rpCompound(
+                id="CMPD_0000000025",
+                smiles="[H]OC(=O)c1c([H])c([H])c([H])c(O[H])c1[H]",
+                inchi="InChI=1S/C7H6O3/c8-6-3-1-2-5(4-6)7(9)10/h1-4,8H,(H,9,10)",
+                inchikey="IJFXRHURBJZNAO-UHFFFAOYSA-N"
+            ),
+            "CMPD_0000000003": rpCompound(
+                id="CMPD_0000000003",
+                smiles="[H]Oc1c([H])c([H])c([H])c([H])c1O[H]",
+                inchi="InChI=1S/C6H6O2/c7-5-3-1-2-4-6(5)8/h1-4,7-8H",
+                inchikey="YCIMNLLNPGFGHC-UHFFFAOYSA-N"
+            ),
+            "MNXM337": rpCompound(
+                id="MNXM337",
+                smiles="[H]OC(=O)C(OC1([H])C([H])=C(C(=O)O[H])C([H])=C([H])C1([H])O[H])=C([H])[H]",
+                inchi="InChI=1S/C10H10O6/c1-5(9(12)13)16-8-4-6(10(14)15)2-3-7(8)11/h2-4,7-8,11H,1H2,(H,12,13)(H,14,15)",
+                inchikey="WTFXTQVDAKGDEY-UHFFFAOYSA-N"
+            ),
+            "MNXM2": rpCompound(
+                id="MNXM2",
+                smiles="[H]O[H]",
+                inchi="InChI=1S/H2O/h1H2",
+                inchikey="XLYOFNOQVPJJNP-UHFFFAOYSA-N"
+            ),
+            "MNXM13": rpCompound(
+                id="MNXM13",
+                smiles="O=C=O",
+                inchi="InChI=1S/CO2/c2-1-3",
+                inchikey="CURLTUGMZLYLDI-UHFFFAOYSA-N",
+                formula="CO2",
+                name="CO2"
+            ),
+            "MNXM5": rpCompound(
+                id="MNXM5",
+                smiles="N=C(O)c1ccc[n+](C2OC(COP(=O)(O)OP(=O)(O)OCC3OC(n4cnc5c(N)ncnc54)C(OP(=O)(O)O)C3O)C(O)C2O)c1",
+                inchi="InChI=1S/C21H28N7O17P3/c22-17-12-19(25-7-24-17)28(8-26-12)21-16(44-46(33,34)35)14(30)11(43-21)6-41-48(38,39)45-47(36,37)40-5-10-13(29)15(31)20(42-10)27-3-1-2-9(4-27)18(23)32/h1-4,7-8,10-11,13-16,20-21,29-31H,5-6H2,(H7-,22,23,24,25,32,33,34,35,36,37,38,39)/p+1",
+                inchikey="XJLXINKUBYWONI-UHFFFAOYSA-O",
+                formula="C21H25N7O17P3",
+                name="NADP(+)"
+            ),
+            "MNXM4": rpCompound(
+                id="MNXM4",
+                smiles="O=O",
+                inchi="InChI=1S/O2/c1-2",
+                inchikey="MYMOFIZGZYHOMD-UHFFFAOYSA-N"
+            ),
+            "MNXM1": rpCompound(
+                id="MNXM1",
+                smiles="[H+]",
+                inchi="InChI=1S/p+1",
+                inchikey="GPRLSGONYQIRFK-UHFFFAOYSA-N"
+            ),
+            "MNXM6": rpCompound(
+                id="MNXM6",
+                smiles="[H]N=C(O[H])C1=C([H])N(C2([H])OC([H])(C([H])([H])OP(=O)(O[H])OP(=O)(O[H])OC([H])([H])C3([H])OC([H])(n4c([H])nc5c(N([H])[H])nc([H])nc54)C([H])(OP(=O)(O[H])O[H])C3([H])O[H])C([H])(O[H])C2([H])O[H])C([H])=C([H])C1([H])[H]",
+                inchi="InChI=1S/C21H30N7O17P3/c22-17-12-19(25-7-24-17)28(8-26-12)21-16(44-46(33,34)35)14(30)11(43-21)6-41-48(38,39)45-47(36,37)40-5-10-13(29)15(31)20(42-10)27-3-1-2-9(4-27)18(23)32/h1,3-4,7-8,10-11,13-16,20-21,29-31H,2,5-6H2,(H2,23,32)(H,36,37)(H,38,39)(H2,22,24,25)(H2,33,34,35)",
+                inchikey="ACFIXJIJDZMPPO-UHFFFAOYSA-N"
+            )
+        }
+
         self.reactants = {
             "CMPD_0000000003": 1,
             "MNXM4": 1
@@ -413,8 +413,6 @@ class Test_rpPathway(TestCase):
         )
 
     def test_rpSBML_rpsbml(self):
-        # print(self.pathway.get_species_ids())
-        # print(self.pathway)
         self.assertEqual(
             self.pathway,
             rpPathway.from_rpSBML(rpsbml=self.pathway.to_rpSBML())
