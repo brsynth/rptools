5b3ad5b022a3f98cd21bf1ed66b090361f9dc2a0
Joan Hérisson <joan.herisson@univ-evry.fr>
2022-02-08 14:28:10 +0100

    chore(rpthermo): add param bounds

--- rptools/rpthermo/Args.py
+++ rptools/rpthermo/Args.py
@@ -3,6 +3,11 @@ from argparse import ArgumentParser
 DEFAULT_pH = 7.5
 DEFAULT_pMg = 3.0
 DEFAULT_ionic_strength = 0.25
+MIN_pH = 0
+MAX_pH = 14
+MIN_ionic_strength = 0
+MAX_ionic_strength = 500
+
 
 def add_arguments(parser: ArgumentParser) -> ArgumentParser:
 
@@ -14,11 +19,15 @@ def add_arguments(parser: ArgumentParser) -> ArgumentParser:
     parser.add_argument(
         '--pH',
         type=float,
+        choices=range(MIN_pH, MAX_pH+1),
+        metavar=f'[{MIN_pH}-{MAX_pH}]',
         default=DEFAULT_pH
     )
     parser.add_argument(
         '--ionic_strength',
         type=float,
+        choices=range(MIN_ionic_strength, MAX_ionic_strength+1),
+        metavar=f'[{MIN_ionic_strength}-{MAX_ionic_strength}]',
         default=DEFAULT_ionic_strength
     )
     parser.add_argument(
