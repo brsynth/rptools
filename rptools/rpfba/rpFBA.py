b2daba40af0175d6379e755569829512f84f21f4
Joan Hérisson <joan.herisson@univ-evry.fr>
2022-06-23 18:08:12 +0200

    chore(rpFBA): auto convert compartment id

--- rptools/rpfba/rpFBA.py
+++ rptools/rpfba/rpFBA.py
@@ -171,8 +171,10 @@ def runFBA(
     )
     if rpsbml_merged is None:
         return None
-    logger.debug('rpsbml_merged: ' + str(rpsbml_merged))
-    logger.debug('reactions_in_both: ' + str(reactions_in_both))
+    logger.debug(f'rpsbml_merged: {rpsbml_merged}')
+    logger.debug(f'reactions_in_both: {reactions_in_both}')
+    logger.debug(f'missing_species: {missing_species}')
+
     cobra_rpsbml_merged = rpSBML.cobraize(rpsbml_merged)
 
     ## MEDIUM
@@ -413,10 +415,12 @@ def check_SBML_compartment(
 
     # Check model compartment ID
     # Set new compartment ID in case it exists under another ID
-    has_compartment = rpsbml.has_compartment(compartment_id)
-    if has_compartment:
-        logger.debug(f'Compartment \'{compartment_id}\' found in the model \'{rpsbml.getName()}\'')
-        return compartment_id
+    compartment = rpsbml.has_compartment(compartment_id)
+    if compartment is not None:
+        logger.debug(f'Compartment \'{compartment_id}\' found in the model \'{rpsbml.getName()}\' as {compartment.getId()}')
+        if compartment_id != compartment.getId():
+            logger.warning(f'Compartment \'{compartment_id}\' has been replaced by {compartment.getId()}')
+        return compartment.getId()
     else:
         logger.error(f'Compartment \'{compartment_id}\' not found in the model \'{rpsbml.getName()}\'')
         logger.error(
@@ -868,10 +872,11 @@ def rp_fraction(
         rxn_id=objective_rxn_id,
         obj_id=f'brs_obj_{objective_rxn_id}',
     )
+    logger.debug(f'objective_id: {objective_id}')
 
-    logger.debug('Optimising the objective: '+str(biomass_rxn_id))
-    logger.debug('     Setting upper bound: '+str(flux*fraction_coeff))
-    logger.debug('     Setting lower bound: '+str(flux*fraction_coeff))
+    logger.debug(f'Optimising the objective: {biomass_rxn_id}')
+    logger.debug(f'     Setting upper bound: {flux*fraction_coeff}')
+    logger.debug(f'     Setting lower bound: {flux*fraction_coeff}')
 
     old_upper_bound, old_lower_bound = rpsbml.getReactionConstraints(biomass_rxn_id)
     rpsbml.setReactionConstraints(
@@ -993,6 +998,8 @@ def build_cobra_model(
     """
 
     rpsbml.logger.info('Creating Cobra object from rpSBML...')
+    logger.debug(f'objective_id: {objective_id}')
+    logger.debug(f'missing_species: {hidden_species}')
 
     rpsbml.activateObjective(
         objective_id = objective_id,
