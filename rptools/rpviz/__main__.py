2e43b37e6938f7ab17fb9bc6ad34761a699bf374
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-29 11:57:14 +0000

    Adding tarfile member sanitization to extractall()

--- rptools/rpviz/__main__.py
+++ rptools/rpviz/__main__.py
@@ -69,7 +69,26 @@ def __run(args):
         elif input_path.is_file() and tarfile.is_tarfile(args.input_rpSBMLs):
             with tempfile.TemporaryDirectory() as tmp_folder:
                 with tarfile.open(args.input_rpSBMLs, mode='r') as tar:
-                    tar.extractall(path=tmp_folder)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar, path=tmp_folder)
                 _ = list(Path(tmp_folder).glob('*.xml'))
                 if not len(_):  # Possible if there is a root folder
                     _ = list(Path(tmp_folder).glob('*/*.xml'))
