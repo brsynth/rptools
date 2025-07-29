ffbce061cdc86e4c410bce0094f0e884344d55bb
Thomas Duigou <thomas.duigou@inrae.fr>
2025-07-29 14:50:28 +0200

    test(rpviz): fix existing tests

--- tests/rpviz/test_cli.py
+++ tests/rpviz/test_cli.py
@@ -1,5 +1,11 @@
-import pytest
+"""Test cases for the rpviz CLI module."""
+import json
+from typing import Dict
 from pathlib import Path
+
+import pytest
+import deepdiff
+
 from rptools.rpviz.__main__ import (
     __build_arg_parser,
     __run,
@@ -9,19 +15,68 @@ from rptools.rpviz.__main__ import (
 REF_IN_DIR = Path(__file__).resolve().parent / 'inputs' / 'as_dir'
 REF_IN_TAR = Path(__file__).resolve().parent / 'inputs' / 'as_tar.tgz'
 REF_OUT_DIR = Path(__file__).resolve().parent / 'outputs'
+COF_FILE = (
+    Path(__file__).resolve().parent.parent.parent
+    / "rptools"
+    / "rpviz"
+    / "data"
+    / "cofactors_mnx_202507.tsv"
+)
 
 
-def __read_and_sort(
-    path: str
-    ) -> list:
-    with open(path) as fh:
-        lines = [_ for _ in fh.readline()]
-    return lines.sort()
+def __dump_file(path: str, data: Dict) -> None:
+    """Dump a dictionary to a file."""
+    with open(path, 'w', encoding='utf-8') as fh:
+        json.dump(data, fh, indent=2, ensure_ascii=False)
+
+
+def __read_and_sort(path: str, skip=[]) -> list:
+    with open(path, encoding='utf-8') as fh:
+        lines = []
+        for line in fh.readlines():
+            if not any(
+                skip_item in line
+                for skip_item in skip
+            ):
+                lines.append(line.strip())
+    return sorted(lines)
+
+
+def __read_multi_object_json(path: str) -> Dict:
+    """Read a JSON-like file containing multiple JSON objects."""
+    objects = {}
+    with open(path, encoding='utf-8') as fh:
+        obj_name = ''
+        obj_content = ''
+        curly_count = 0
+        for line in fh:
+            for char in line.strip():
+                if char == '{':
+                    if curly_count == 0:
+                        # Start of a new object
+                        obj_name = line.strip().split()[0]
+                        obj_content = ''
+                    obj_content += char
+                    curly_count += 1
+                elif char == '}':
+                    obj_content += char
+                    if curly_count == 1:
+                        # End of the current object
+                        objects[obj_name] = json.loads(obj_content)
+                        obj_name = ''
+                        obj_content = ''
+                    curly_count -= 1
+                else:
+                    obj_content += char
+        # Last object
+        if curly_count == 0 and obj_content:
+            objects[obj_name] = json.loads(obj_content)
+    return objects
 
 
 def test_build_arg_parser(mocker):
-    # No args
-    args = ['prog']
+    """Test the argument parser."""
+    args = ['prog']  # no arguments
     mocker.patch('sys.argv', args)
     parser = __build_arg_parser()
     with pytest.raises(SystemExit):
@@ -29,24 +84,28 @@ def test_build_arg_parser(mocker):
 
 
 def test_dir_input(mocker, tmpdir):
-    out_dir = tmpdir / 'odir'
-    args = ['prog', str(REF_IN_DIR), str(out_dir)]
+    """Test the CLI with a directory input."""
+    args = ['prog', str(REF_IN_DIR), str(tmpdir)]
     mocker.patch('sys.argv', args)
     parser = __build_arg_parser()
     args = parser.parse_args()
     __run(args)
     ref_file = REF_OUT_DIR / 'network.json'
-    test_file = out_dir / 'network.json'
-    assert __read_and_sort(test_file) == __read_and_sort(ref_file)
+    test_file = tmpdir / 'network.json'
+    ref_objects = __read_multi_object_json(ref_file)
+    test_objects = __read_multi_object_json(test_file)
+    assert not deepdiff.DeepDiff(ref_objects, test_objects, ignore_order=True)
 
 
 def test_tar_input(mocker, tmpdir):
-    out_dir = tmpdir / 'odir'
-    args = ['prog', str(REF_IN_TAR), str(out_dir)]
+    """Test the CLI with a tar input."""
+    args = ['prog', str(REF_IN_TAR), str(tmpdir)]
     mocker.patch('sys.argv', args)
     parser = __build_arg_parser()
     args = parser.parse_args()
     __run(args)
     ref_file = REF_OUT_DIR / 'network.json'
-    test_file = out_dir / 'network.json'
-    assert __read_and_sort(test_file) == __read_and_sort(ref_file)
+    test_file = tmpdir / 'network.json'
+    ref_objects = __read_multi_object_json(ref_file)
+    test_objects = __read_multi_object_json(test_file)
+    assert not deepdiff.DeepDiff(ref_objects, test_objects, ignore_order=True)
