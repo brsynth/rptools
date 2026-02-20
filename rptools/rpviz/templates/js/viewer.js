47e4f330c48bad6c5dd6eb2a97b75f9de2554b2b
Joan Hérisson <joan.herisson@univ-evry.fr>
2026-02-20 15:40:18 +0100

    feat (rpviz): Toggle panels

--- rptools/rpviz/templates/js/viewer.js
+++ rptools/rpviz/templates/js/viewer.js
@@ -831,7 +831,7 @@ $(function(){
         // Playing with zoom to get the best fit
         cy.minZoom(1e-50);
         cy.on('layoutstop', function(e){
-            cy.minZoom(cy.zoom()*0.9);  // 0.9 to enable the user dezoom a little
+            cy.minZoom(1e-50);  // Allow full zoom-out range
         });
         // Layout
         let layout = element_collection.layout({
